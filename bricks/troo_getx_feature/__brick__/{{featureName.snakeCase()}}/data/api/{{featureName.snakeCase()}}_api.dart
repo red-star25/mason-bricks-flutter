ef02ace2a75cb8ef277e3d15311fd72596431833
dhruv-nakum <dhruv.nakum@trootech.com>
2022-08-25 18:44:17 +0530

    - getx structure added mustach in api file - resolved pubspec.yaml mustach syntax

--- bricks/troo_getx_feature/__brick__/{{featureName.snakeCase()}}/data/api/{{featureName.snakeCase()}}_api.dart
+++ bricks/troo_getx_feature/__brick__/{{featureName.snakeCase()}}/data/api/{{featureName.snakeCase()}}_api.dart
@@ -1,18 +1,9 @@
 import 'package:dio/dio.dart';
 
-class HomeApi {
+class {{featureName.pascalCase()}}Api {
   // dio instance
   final DioClient _dioClient;
 
   // injecting dio instance
-  HomeApi(this._dioClient);
-
-  Future<Map<String, dynamic>> getUserApi() async {
-    try {
-      final Response response = await _dioClient.get('');
-      return response.data;
-    } catch (e) {
-      rethrow;
-    }
-  }
+  {{featureName.pascalCase()}}Api(this._dioClient);
 }
